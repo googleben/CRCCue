e28440bd8b2acd1bf72442abbf828cc4d9c5776e
MonsterDruide1 <5958456@gmail.com>
2020-03-01 14:51:55 +0100

    Moving default values to HTML file

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 var sqs = [];
-var numSq = 8;
+var numberOfCircles;
 // var bpm = 116;
-// var msTotal = 1*60*1000 / bpm * 7;
-var msTotal = 3102;
-var msBetween = msTotal / (numSq - 1);
-var n = numSq - 1;
-var timeIn;
-var circlesIn;
+// var totalLength = 1*60*1000 / bpm * 7;
+var totalLength;
+var msBetween = totalLength / (numberOfCircles - 1);
+var n = numberOfCircles - 1;
 
 var runN = 0;
 var running = false;
@@ -14,48 +12,32 @@ var sameBox = false;
 
 
 window.addEventListener('load', () => {
-
     loadBuffers();
-
-    timeIn = document.getElementById("totalMs");
-    timeIn.value = msTotal;
-    circlesIn = document.getElementById("numBoxes");
-    circlesIn.value = numSq;
-
-    var start = document.getElementById("container");
-    var same = document.getElementById("same").checked;
-    makeSqs();
-    if (same) {
-        msBetween = msTotal / (numSq);
-        sameBox = true;
-    } else sameBox = false;
-
+    reset();
+    
     //scheduleRun(runN);
-
 });
 
 function run(a) {
     if (a != runN) return;
 
-
-
-    if (n >= numSq) n = numSq - 1;
+    if (n >= numberOfCircles) n = numberOfCircles - 1;
     sqs[n].className = "timer red";
     n++;
-    if (n == numSq) n = 0;
+    if (n == numberOfCircles) n = 0;
     sqs[n].className = "timer green";
     if (document.getElementById("useSound").checked) {
         var kickVol = document.getElementById("kickVol").value / 100;
         var snareVol = document.getElementById("snareVol").value / 100;
-        if (n == 0 || (n == (numSq - 1) && !sameBox)) playSound("kick", kickVol);
-        if (document.getElementById("useTriplet").checked && (n == (numSq - 1) || (n == (numSq - 2) && !sameBox))) {
+        if (n == 0 || (n == (numberOfCircles - 1) && !sameBox)) playSound("kick", kickVol);
+        if (document.getElementById("useTriplet").checked && (n == (numberOfCircles - 1) || (n == (numberOfCircles - 2) && !sameBox))) {
             playSound("snare", snareVol, msBetween / 3000);
             playSound("snare", snareVol, msBetween * 2 / 3000);
         }
-        if (document.getElementById("useEighth").checked && (n == (numSq - 1) || (n == (numSq - 2) && !sameBox))) {
+        if (document.getElementById("useEighth").checked && (n == (numberOfCircles - 1) || (n == (numberOfCircles - 2) && !sameBox))) {
             playSound("snare", snareVol, msBetween / 2000);
         }
-        if (document.getElementById("useSixteenth").checked && (n == (numSq - 1) || (n == (numSq - 2) && !sameBox))) {
+        if (document.getElementById("useSixteenth").checked && (n == (numberOfCircles - 1) || (n == (numberOfCircles - 2) && !sameBox))) {
             playSound("snare", snareVol, msBetween / 4000);
             playSound("snare", snareVol, msBetween * 2 / 4000);
             playSound("snare", snareVol, msBetween * 3 / 4000);
@@ -72,16 +54,16 @@ function stop() {
 }
 
 function reset() {
-    msTotal = timeIn.value;
-    numSq = circlesIn.value;
-    msBetween = msTotal / (numSq - 1);
+    totalLength = document.getElementById("totalMs").value;
+    numberOfCircles = document.getElementById("numBoxes").value;
+    msBetween = totalLength / (numberOfCircles - 1);
 
     for (var sq of sqs) sq.remove();
 
     makeSqs();
-    n = numSq - 1;
+    n = numberOfCircles - 1;
     if (document.getElementById("same").checked) {
-        msBetween = msTotal / (numSq);
+        msBetween = totalLength / (numberOfCircles);
         sameBox = true;
     } else sameBox = false;
 
@@ -94,11 +76,11 @@ function scheduleRun(a) {
 function makeSqs() {
     var start = document.getElementById("container");
     var same = document.getElementById("same").checked;
-    for (var x = 0; x < numSq; x++) {
+    for (var x = 0; x < numberOfCircles; x++) {
         sqs[x] = document.createElement("div");
         sqs[x].className = "timer red";
         start.appendChild(sqs[x]);
-        if (x == 0 || (x == numSq - 1 && !same)) {
+        if (x == 0 || (x == numberOfCircles - 1 && !same)) {
             var p = document.createElement("p");
             sqs[x].appendChild(p);
             p.textContent = "Y"
