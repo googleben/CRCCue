ba7a5a1d49ce2e086ded2d0ee593b0e898b14d8a
MonsterDruide1 <5958456@gmail.com>
2020-03-01 14:17:29 +0100

    Cleanup
    Proper indentations

---
+++ fileManager.js
@@ -0,0 +1,98 @@
+// Keep track of all loaded buffers.
+var BUFFERS = {};
+// Page-wide audio context.
+var context = new AudioContext();;
+
+function BufferLoader(context, urlList, callback) {
+    this.context = context;
+    this.urlList = urlList;
+    this.onload = callback;
+    this.bufferList = new Array();
+    this.loadCount = 0;
+}
+
+BufferLoader.prototype.loadBuffer = function(url, index) {
+    // Load buffer asynchronously
+    var request = new XMLHttpRequest();
+    request.open("GET", url, true);
+    request.responseType = "arraybuffer";
+
+    var loader = this;
+
+    request.onload = function() {
+        // Asynchronously decode the audio file data in request.response
+        loader.context.decodeAudioData(
+            request.response,
+            function(buffer) {
+                if (!buffer) {
+                    alert('error decoding file data: ' + url);
+                    return;
+                }
+                loader.bufferList[index] = buffer;
+                if (++loader.loadCount == loader.urlList.length)
+                    loader.onload(loader.bufferList);
+            },
+            function(error) {
+                console.error('decodeAudioData error', error);
+            }
+        );
+    }
+
+    request.onerror = function() {
+        alert('BufferLoader: XHR error');
+    }
+
+    request.send();
+}
+
+BufferLoader.prototype.load = function() {
+    for (var i = 0; i < this.urlList.length; ++i)
+        this.loadBuffer(this.urlList[i], i);
+}
+
+
+// An object to track the buffers to load {name: path}
+var BUFFERS_TO_LOAD = {
+    kick: 'kick.wav',
+    snare: 'snare.wav',
+};
+
+// Loads all sound samples into the buffers object.
+function loadBuffers() {
+    // Array-ify
+    var names = [];
+    var paths = [];
+    for (var name in BUFFERS_TO_LOAD) {
+        var path = BUFFERS_TO_LOAD[name];
+        names.push(name);
+        paths.push(path);
+
+    }
+    bufferLoader = new BufferLoader(context, paths, function(bufferList) {
+        for (var i = 0; i < bufferList.length; i++) {
+            var buffer = bufferList[i];
+            var name = names[i];
+            BUFFERS[name] = buffer;
+        }
+    });
+    bufferLoader.load();
+}
+
+function playSound(sound, vol, time) {
+    if (!time) time = 0;
+    if (!vol) vol = 1;
+    if (!sound) sound = "snare";
+    var kick = BUFFERS.kick;
+    var snare = BUFFERS.snare;
+    var buffer;
+    if (sound === "kick") buffer = kick;
+    if (sound === "snare") buffer = snare;
+    var source = context.createBufferSource();
+    source.buffer = buffer;
+    var gainNode = context.createGain();
+    source.connect(gainNode);
+    gainNode.gain.value = vol;
+    gainNode.connect(context.destination);
+
+    source.start(context.currentTime + time);
+}
---
+++ main.js
@@ -0,0 +1,106 @@
+var sqs = [];
+var numSq = 8;
+// var bpm = 116;
+// var msTotal = 1*60*1000 / bpm * 7;
+var msTotal = 3102;
+var msBetween = msTotal / (numSq - 1);
+var n = numSq - 1;
+var timeIn;
+var circlesIn;
+
+var runN = 0;
+var running = false;
+var sameBox = false;
+
+function run(a) {
+    if (a != runN) return;
+
+
+
+    if (n >= numSq) n = numSq - 1;
+    sqs[n].className = "timer red";
+    n++;
+    if (n == numSq) n = 0;
+    sqs[n].className = "timer green";
+    if (document.getElementById("useSound").checked) {
+        var kickVol = document.getElementById("kickVol").value / 100;
+        var snareVol = document.getElementById("snareVol").value / 100;
+        if (n == 0 || (n == (numSq - 1) && !sameBox)) playSound("kick", kickVol);
+        if (document.getElementById("useTriplet").checked && (n == (numSq - 1) || (n == (numSq - 2) && !sameBox))) {
+            playSound("snare", snareVol, msBetween / 3000);
+            playSound("snare", snareVol, msBetween * 2 / 3000);
+        }
+        if (document.getElementById("useEighth").checked && (n == (numSq - 1) || (n == (numSq - 2) && !sameBox))) {
+            playSound("snare", snareVol, msBetween / 2000);
+        }
+        if (document.getElementById("useSixteenth").checked && (n == (numSq - 1) || (n == (numSq - 2) && !sameBox))) {
+            playSound("snare", snareVol, msBetween / 4000);
+            playSound("snare", snareVol, msBetween * 2 / 4000);
+            playSound("snare", snareVol, msBetween * 3 / 4000);
+        }
+        playSound("snare", snareVol);
+    }
+    scheduleRun(a);
+}
+
+function stop() {
+    runN++;
+    running = false;
+    for (var sq of sqs) sq.className = "timer red";
+}
+
+function scheduleRun(a) {
+    window.setTimeout(run, msBetween, a);
+}
+
+function makeSqs() {
+    var start = document.getElementById("container");
+    var same = document.getElementById("same").checked;
+    for (var x = 0; x < numSq; x++) {
+        sqs[x] = document.createElement("div");
+        sqs[x].className = "timer red";
+        start.appendChild(sqs[x]);
+        if (x == 0 || (x == numSq - 1 && !same)) {
+            var p = document.createElement("p");
+            sqs[x].appendChild(p);
+            p.textContent = "Y"
+        }
+    }
+}
+
+window.addEventListener('load', () => {
+
+    loadBuffers();
+
+    timeIn = document.getElementById("totalMs");
+    timeIn.value = msTotal;
+    circlesIn = document.getElementById("numBoxes");
+    circlesIn.value = numSq;
+
+    var start = document.getElementById("container");
+    var same = document.getElementById("same").checked;
+    makeSqs();
+    if (same) {
+        msBetween = msTotal / (numSq);
+        sameBox = true;
+    } else sameBox = false;
+
+    //scheduleRun(runN);
+
+});
+
+function reset() {
+    msTotal = timeIn.value;
+    numSq = circlesIn.value;
+    msBetween = msTotal / (numSq - 1);
+
+    for (var sq of sqs) sq.remove();
+
+    makeSqs();
+    n = numSq - 1;
+    if (document.getElementById("same").checked) {
+        msBetween = msTotal / (numSq);
+        sameBox = true;
+    } else sameBox = false;
+
+}
